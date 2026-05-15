75a9718dc2e65c34c9aa354399ccf52f09d3afc1
AinurTulkas <m.delarosa.atik.tech@gmail.com>
2026-05-15 13:31:52 -0600

    feat: add sw.js for offline support

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'dnd-toolkit-v1';
+const assetsToCache = [
+    './',
+    './index.html',
+    './app.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(assetsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
