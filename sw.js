07617aac2c284e61bf885890ec39caa192cede51
AinurTulkas <m.delarosa.atik.tech@gmail.com>
2026-06-05 18:29:24 -0600

    Service Worker v2: Forzar actualización de cache para ver cambios

--- sw.js
+++ sw.js
@@ -1,15 +1,32 @@
-const CACHE_NAME = 'dnd-toolkit-v1';
+const CACHE_NAME = 'dnd-toolkit-v2'; // Incrementado a v2 para forzar actualización
 const assetsToCache = [
     './',
     './index.html',
+    './styles.css',
     './app.js',
-    './manifest.json'
+    './manifest.json',
+    './data/srd_data.json'
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting(); // Forzar a que el nuevo SW tome el control inmediatamente
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(assetsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName); // Limpiar versiones viejas
+                    }
+                })
+            );
+        })
     );
 });
 
